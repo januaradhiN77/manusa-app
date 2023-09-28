7b05f672c5a562fb211b9e8a962e1a18df4ce8bb
h <h>
2023-09-28 10:39:34 +0700

    g

--- main.js
+++ main.js
@@ -2,3 +2,18 @@ function goToWebsite(websiteURL) {
       window.location.href = websiteURL;
       //alert("Oops! Terjadi kesalahan. 4000");
     }
+
+const allSkeleton = document.querySelectorAll('.skeleton');
+const allContent =  document.getElementById('skel');
+
+
+window.addEventListener('load', function() {
+  setTimeout(function() {
+    allSkeleton.forEach(item => {
+      item.classList.remove('skeleton');
+      allContent.style.opacity = "1";
+    });
+  }, 700);
+});
+
+setTimeout()
